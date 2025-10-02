f0af2146d04ddb5187d68a14bdda8a48f4a34852
Vinicius Bertolo <vini.alex.bertolo@gmail.com>
2025-10-02 18:40:44 -0300

    space mobile

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -77,18 +77,18 @@ const itemVariants = {
 const Hero = () => {
   return (
     // O <main> agora é um elemento normal, sem animação
-    <main className="flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(90vh-6rem)]">
+    <main className="flex lg:mt-20 flex-col lg:flex-row items-center justify-between min-h-[calc(80vh-6rem)]">
 
       {/* A animação em cascata começa AQUI */}
       <motion.div 
-        className="max-w-xl ml-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0"
+        className="max-w-xl ml-[5%] mr-[5%] z-10 mt-[90%] md:mt-[60%] lg:mt-0"
         variants={containerVariants}
         initial="hidden"
         animate="visible"
       >
         
         {/* Cada filho direto se torna um 'motion' e recebe a animação 'itemVariants' */}
-        <motion.div variants={itemVariants} className='relative w-[95%] sm:w-48 h-10 bg-gradient-to-r from-[#ff8c00] to-[#ff0080] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full'>
+        <motion.div variants={itemVariants} className='relative w-[100%] sm:w-48 h-10 bg-gradient-to-r from-[#ff8c00] to-[#ff0080] shadow-[0_0_15px_rgba(255,255,255,0.4)] rounded-full'>
           <div className='font-[Open_Sans] absolute inset-[3px] bg-black rounded-full flex items-center justify-center gap-1' >
             <i className='bx bx-diamond'>
                 </i>
@@ -106,11 +106,11 @@ const Hero = () => {
           The best way to reach humans instead of spam folders, deliver transactional and marketing emails at scale.
         </motion.p>
 
-        <motion.div variants={itemVariants} className='flex gap-4 mt-12'>
+        <motion.div variants={itemVariants} className='flex gap-4 mt-6'>
           <a className='font-[Open_Sans] border border-[#2a2a2a] py-2 sm:py-3 px-4 sm:px-5 rounded-full sm:text-lg text-sm font-semibold tracking-wider transition-all duration-300 hover:bg-[#1a1a1a]' href="#">
             Documentation <i className='bx bx-link-external'></i>
           </a>
-          <a className='font-[Open_Sans] border border-[#2a2a2a] py-2 sm:py-3 px-8 sm:px-10 rounded-full sm:text-lg text-sm font-semibold tracking-wider transition-all duration-300 hover:bg-[#1a1a1a] bg-gray-300 text-black hover:text-white' href="#">
+          <a className='font-[Open_Sans] py-2 sm:py-3 px-8 sm:px-10 rounded-full sm:text-lg text-sm font-semibold tracking-wider transition-all duration-300 hover:bg-[#1a1a1a] bg-[#ff8c00] text-black hover:text-white' href="#">
             Get Started <i className='bx bx-link-external'></i>
           </a>
         </motion.div>
